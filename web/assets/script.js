9557697ec41ff30428368271d53fa613c148b0b9
Carr O'Connor <carroconnor@c2itconsulting.net>
2022-04-21 22:50:18 -0400

    icons

--- web/assets/script.js
+++ web/assets/script.js
@@ -1,5 +1,4 @@
 window.onscroll = function() {
-    let menuBtn = document.querySelector(".menuBtn");
     var nav = document.getElementById('navId');
 
     //header image bottom
@@ -63,15 +62,12 @@ function currentSlide(n) {
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("gallerySlides");
-    // var dots = document.getElementsByClassName("dot");
+
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    // for (i = 0; i < dots.length; i++) {
-    //     dots[i].className = dots[i].className.replace(" active", "");
-    // }
+
     slides[slideIndex-1].style.display = "block";
-    // dots[slideIndex-1].className += " active";
 }
